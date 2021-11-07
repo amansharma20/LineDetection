fc6bb872135207bebffa82bb37aa5ea8a79ab856
Aman <amanacts20@gmail.com>
2021-11-07 16:12:10 +0530

    Count number of Lines

--- App.js
+++ App.js
@@ -1,11 +1,113 @@
+import React, { useRef, useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  Pressable,
+  Modal,
+  Text,
+  ActivityIndicator,
+} from 'react-native';
+
+import {
+  getModel,
+  convertBase64ToTensor,
+  startPrediction,
+} from './src/helpers/tensor-helper';
+
+import { Camera } from 'expo-camera';
+
+import { cropPicture } from './src/helpers/image-helper';
+
+const RESULT_MAPPING = ['ONE LINE', 'TWO LINES', 'THREE LINES', 'FOUR LINES'];
 
 export default function App() {
+
+  const cameraRef = useRef();
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [presentedShape, setPresentedShape] = useState('');
+
+  const [hasPermission, setHasPermission] = useState(null);
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  const handleImageCapture = async () => {
+    setIsProcessing(true);
+    const imageData = await cameraRef.current.takePictureAsync({
+      base64: true,
+    });
+    processImagePrediction(imageData);
+  };
+
+  const processImagePrediction = async (base64Image) => {
+    const croppedData = await cropPicture(base64Image, 300);
+    const model = await getModel();
+    const tensor = await convertBase64ToTensor(croppedData.base64);
+
+    const prediction = await startPrediction(model, tensor);
+    console.log('prediction');
+    console.log(prediction);
+    console.log('prediction');
+
+    const highestPrediction = prediction.indexOf(
+      Math.max.apply(null, prediction),
+    );
+    console.log('highestPrediction');
+    console.log(highestPrediction);
+    console.log('highestPrediction');
+    setPresentedShape(RESULT_MAPPING[highestPrediction]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Modal visible={isProcessing} transparent={true} animationType="slide">
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            {console.log(presentedShape)}
+            <Text>Your current number of lines is {presentedShape}</Text>
+            {presentedShape === '' &&
+              (
+                <Text>
+                  Loading...
+                </Text>
+              )
+            }
+            <Pressable
+              style={styles.dismissButton}
+              onPress={() => {
+                setPresentedShape('');
+                setIsProcessing(false);
+              }}>
+              <Text>Dismiss</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
+      <Camera
+        ref={cameraRef}
+        style={styles.camera}
+        type={Camera.Constants.Type.back}
+        autoFocus={true}
+        whiteBalance={Camera.Constants.WhiteBalance.auto}
+      />
+      <Pressable
+        onPress={() => handleImageCapture()}
+        style={styles.captureButton}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +117,48 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    width: '100%',
+    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  camera: {
+    width: '100%',
+    height: '100%',
+  },
+  captureButton: {
+    position: 'absolute',
+    left: Dimensions.get('screen').width / 2 - 50,
+    bottom: 40,
+    width: 100,
+    zIndex: 100,
+    height: 100,
+    backgroundColor: 'black',
+    borderRadius: 50,
+  },
+  modal: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalContent: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 300,
+    height: 300,
+    borderRadius: 24,
+    backgroundColor: 'gray',
+  },
+  dismissButton: {
+    width: 150,
+    height: 50,
+    marginTop: 60,
+    borderRadius: 24,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'red',
   },
 });
